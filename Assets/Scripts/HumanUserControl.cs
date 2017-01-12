234d389484174fe48b0ce30af11b2211d2b8f7af
Nuozhou Xu <nuozhouxu@MacBook-Pro-10.local>
2017-01-11 23:32:13 -0600

    add ps4 controller support

---
+++ Assets/Scripts/HumanUserControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (HumanMovements))]
+public class HumanUserControl : MonoBehaviour {
+
+	private HumanMovements m_Character;
+
+	private bool m_Jump;
+	private bool crouch;
+
+
+	private void Awake()
+	{
+		m_Character = GetComponent<HumanMovements>();
+	}
+
+
+	private void Update()
+	{
+		if (!m_Jump)
+		{
+			// Read the jump input in Update so button presses aren't missed.
+			m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			crouch = CrossPlatformInputManager.GetButton ("Crouch");
+		}
+	}
+
+
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		//bool crouch = Input.GetKey(KeyCode.LeftControl);
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		// Pass all parameters to the character control script.
+		m_Character.Move(h, crouch, m_Jump);
+		m_Jump = false;
+	}
+}

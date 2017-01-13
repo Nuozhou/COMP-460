a871ebb01d8234572649357330c8e7c0ee422780
Nuozhou Xu <nuozhouxu@MacBook-Pro-10.local>
2017-01-13 14:45:55 -0600

    added alien

--- Assets/Scripts/HumanUserControl.cs
+++ Assets/Scripts/HumanUserControl.cs
@@ -22,8 +22,8 @@ public class HumanUserControl : MonoBehaviour {
 		if (!m_Jump)
 		{
 			// Read the jump input in Update so button presses aren't missed.
-			m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-			crouch = CrossPlatformInputManager.GetButton ("Crouch");
+			m_Jump = CrossPlatformInputManager.GetButtonDown("HumanJump");
+			crouch = CrossPlatformInputManager.GetButton ("HumanCrouch");
 		}
 	}
 
@@ -32,7 +32,7 @@ public class HumanUserControl : MonoBehaviour {
 	{
 		// Read the inputs.
 		//bool crouch = Input.GetKey(KeyCode.LeftControl);
-		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		float h = CrossPlatformInputManager.GetAxis("HumanHorizontal");
 		// Pass all parameters to the character control script.
 		m_Character.Move(h, crouch, m_Jump);
 		m_Jump = false;

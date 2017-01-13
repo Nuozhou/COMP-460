f34d15169069c84bd4b94cf011b74f34238a2ba0
Nuozhou Xu <nuozhouxu@MacBook-Pro-10.local>
2017-01-13 14:00:31 -0600

    edited human script

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gm;
+
+	void Start() {
+		if (gm == null) {
+			gm = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameMaster>();
+		}
+	}
+
+	public Transform HumanPrefab;
+	public Transform SavePoint;
+
+	public static void KillHuman(Human human) {
+		human.gameObject.transform.position = gm.SavePoint.position;
+		human.health = 100;
+	}
+}
---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour {
+
+	public int health = 100;
+
+	public int fallBoundary = -100;
+
+	public void DamageHuman(int damage) {
+		health -= damage;
+		if (health <= 0) {
+			GameMaster.KillHuman (this);
+		}
+	}
+
+	void Update() {
+		if (transform.position.y <= fallBoundary)
+			DamageHuman(1000);
+	}
+}

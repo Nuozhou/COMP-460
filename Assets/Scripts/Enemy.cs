34f4f41674562c49cea82b6bfca8f528dd8eefa6
yukichul <yukichul@yahoo.com>
2017-01-20 21:11:13 -0600

    create health bar for enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	[SerializeField]
 	public int health = 100;
+
+	[SerializeField]
+	private Stat healthStat;
 	// Use this for initialization
 	public GameObject cloud;
 
 	public Renderer rend;
 
+	private Canvas healthCanvas;
+
 	public void Start() {
 		rend = GetComponent<Renderer>();
+		healthStat.Initialize ();
+		healthCanvas = GetComponentInChildren<Canvas> ();
+		healthCanvas.enabled = false;
+
 	}
 	public void Damage(int healthDecrease) {
-		health -= healthDecrease;
-		if (health <= 0) {
+		if (!healthCanvas.isActiveAndEnabled) {
+			StartCoroutine(ShowBar ());
+		}
+		healthStat.CurrentVal -= healthDecrease;
+		if (healthStat.CurrentVal <= 0) {
 			
 			if (cloud == null) {
 				cloud = (GameObject)Instantiate (Resources.Load ("Cloud"), transform.position, transform.rotation);
@@ -28,6 +41,12 @@ public class Enemy : MonoBehaviour {
 		} else {
 			StartCoroutine (Blink (8.0));
 		}
+	}
+
+	IEnumerator ShowBar() {
+		healthCanvas.enabled = true;
+		yield return new WaitForSeconds (1f);
+		healthCanvas.enabled = false;
 	}
 
 	IEnumerator Blink(double time) {

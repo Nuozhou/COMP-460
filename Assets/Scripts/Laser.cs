be84c1ef26a429fcf575325e462495360f2e4ce8
yukichul <yukichul@yahoo.com>
2017-02-05 12:45:00 -0600

    commit

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+	
+	LineRenderer line;
+	//public Transform laserHit;
+	int n = 2;
+
+	void Start() {
+		line = transform.GetComponent<LineRenderer> ();
+		line.enabled = true;
+		line.useWorldSpace = true;
+	}
+
+	void Update() {
+		
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, -transform.up);
+		Debug.DrawLine (transform.position, hit.point);
+		//laserHit.position = hit.point;
+		line.SetPosition (0, transform.position);
+		line.SetPosition (1, hit.point);
+		if (hit.transform.tag == "Player") {
+			hit.transform.GetComponent<Human> ().DamageHuman (5);
+		}
+	}
+
+}

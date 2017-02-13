64e53d021a08b9bb0af5f7efb7f7b4c4cfb26ee6
Nuozhou Xu <nuozhouxu@MacBook-Pro-10.local>
2017-02-13 14:24:19 -0600

    fixed bug

--- Assets/Scripts/Force.cs
+++ Assets/Scripts/Force.cs
@@ -57,7 +57,7 @@ public class Force : MonoBehaviour {
 					float minDistance = Vector2.Distance (new Vector2 (grabbedObject.transform.position.x, grabbedObject.transform.position.y),
 						                    new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y));
 					foreach (Collider2D col in colliderArray) {
-						if (col.gameObject.tag == "Throwable" || col.gameObject.tag == "Enemy" || colliderArray [i].gameObject.tag == "Grabbable") {
+						if (col.gameObject.tag == "Throwable" || col.gameObject.tag == "Enemy" || col.gameObject.tag == "Grabbable") {
 							float distance = Vector2.Distance (new Vector2 (col.gameObject.transform.position.x, col.gameObject.transform.position.y),
 								                 new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y));
 							if (distance < minDistance) {

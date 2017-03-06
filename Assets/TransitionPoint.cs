1dc2562cb656f4b5eb9cc99078a6eb1f5e226902
Nuozhou Xu <nuozhouxu@MacBook-Pro-10.local>
2017-03-06 12:36:43 -0600

    some bgm

---
+++ Assets/TransitionPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionPoint : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player" && col.name == "Human") {
+			SceneManager.LoadScene (10);
+		}
+	}
+}
